21de004900596b5b0772d04bf3ff8a4246edf25b
SushWar <tanwarsushant27@gmail.com>
2024-01-16 21:05:32 +0530

    first commit

---
+++ Backend-Soul/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+const port = 8000
+const app = express()
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" })
+})
+
+app.listen(port, () => {
+  console.log(`Server is runnning on ${port} port`)
+})
